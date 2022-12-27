92681118808b7d59914d5bbbabe50a5ee3b2d66c
kaiovasa1999-ux <veselin.d.valkanov@gmail.com>
2022-12-27 10:26:07 +0200

    implement the base routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import "./App.css";
+import { useState } from "react";
+import GameCatalog from "./components/GameCatalog";
 import Header from "./components/Header";
 import WelcomeWorld from "./components/WelcomeWorld";
+import CreateGame from "./components/CreateGame";
+import ErrorPage from "./components/ErrorPage";
 
 function App() {
+  const [page, setPage] = useState("/home");
+  const router = {
+    "/home": <WelcomeWorld />,
+    "/games": <GameCatalog />,
+    "/createGame": <CreateGame />,
+  };
+
+  const handlerNavigator = (path) => {
+    setPage(path);
+  };
   return (
     <div id="box">
-      <Header />
-      <main id="main-content">
-        <WelcomeWorld />
-      </main>
+      <Header handlerNavigator={handlerNavigator} />
+      <main id="main-content">{router[page] || <ErrorPage />}</main>
     </div>
   );
 }
