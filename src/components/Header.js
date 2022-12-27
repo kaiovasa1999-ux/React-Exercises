f9a5516acc4a277741f56643022f8bb2465ea09c
kaiovasa1999-ux <veselin.d.valkanov@gmail.com>
2022-12-27 09:55:45 +0200

    demo router

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,38 @@
 import React from "react";
+import CreateGame from "./CreateGame";
 
 function Header() {
+  const onHeaderClick = (e) => {
+    e.preventDefault();
+
+    if (e.target.tagName == "A") {
+      let url = new URL(e.target.href);
+      console.log(url.pathname);
+    }
+  };
+  const router = {
+    "/home": <Header />,
+    "/createGame": <CreateGame />,
+  };
   return (
-    <div>
-      <header>
-        <h1>
-          <a className="home" href="#">
-            GamesPlay
-          </a>
-        </h1>
-        <nav>
-          <a href="#">All games</a>
-          <div id="user">
-            <a href="#">Create Game</a>
-            <a href="#">Logout</a>
-          </div>
-          <div id="guest">
-            <a href="#">Login</a>
-            <a href="#">Register</a>
-          </div>
-        </nav>
-      </header>
-    </div>
+    <header onClick={onHeaderClick}>
+      <h1>
+        <a className="home" href="/home">
+          GamesPlay
+        </a>
+      </h1>
+      <nav>
+        <a href="/games">All games</a>
+        <div id="user">
+          <a href="/createGame">Create Game</a>
+          <a href="/logout">Logout</a>
+        </div>
+        <div id="guest">
+          <a href="/login">Login</a>
+          <a href="/register">Register</a>
+        </div>
+      </nav>
+    </header>
   );
 }
 
